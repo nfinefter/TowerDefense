2ef9db539a3ec32c4370548a9f367a22ab188193
Nicholas Finefter <nfinefter@gmail.com>
2023-07-29 14:37:08 -0700

    Made Screens

--- TowerDefense/Screens.cs
+++ TowerDefense/Screens.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,61 @@ using System.Threading.Tasks;
 
 namespace TowerDefense
 {
-    internal class Screens
+    public class MenuScreen : Screen
     {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Screenum Update(GameTime gameTime)
+        {
+            if (Buttons[0].Pos.Contains(Mouse.GetState().Position) && ScreenManager.Instance.CurrentState == ButtonState.Pressed && ScreenManager.Instance.PrevState == ButtonState.Released)
+            {
+                return Screenum.Game;
+            }
+            return Screenum.Menu;
+        }
     }
+    public class GameScreen : Screen
+    {
+        public List<Player> monkeys = new List<Player>();
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            void DrawProjectiles()
+            {
+                for (int i = 0; i < Player.projectiles.Count; i++)
+                {
+                    var temp = Player.projectiles[i].Rotation;
+                    Player.projectiles[i].Rotation -= 90;
+                    Player.projectiles[i].Draw(spriteBatch);
+                    Player.projectiles[i].Rotation += 90;
+                }
+
+            }
+
+            for (int i = 0; i < Sprites.Count; i++)
+            {
+                Sprites[i].Draw(spriteBatch);
+            }
+            for (int i = 0; i < Buttons.Count; i++)
+            {
+                Buttons[i].Draw(spriteBatch);
+            }
+
+
+            DrawProjectiles();
+        }
+
+        public override Screenum Update(GameTime gameTime)
+        {
+            for (int i = 0; i < Buttons.Count; i++)
+            {
+                Buttons[i].Update(gameTime);
+            }
+            return Screenum.Game;
+        }
+    }
+
 }
